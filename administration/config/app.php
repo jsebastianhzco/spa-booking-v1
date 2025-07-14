5c869d26de182ab2280681724021bc613d4386dc
Johan <jsebastianhzco@gmail.com>
2025-07-13 21:18:25 -0500

    Legacy version - Initial commit

---
+++ administration/config/app.php
@@ -0,0 +1,78 @@
+<?php
+$servername ="localhost";
+
+
+/*
+$db = "acupuncture_reservations";
+$username = "root";
+$password = "";
+*/
+
+$db = "acupuncture_reservations";
+$username = "acupuncture_wp1";
+$password = "N^pD5e3LBZ#@(vUs#h)84&*1";
+
+
+
+
+
+$data = array(
+
+
+
+        "Nombre"=>"Gestion de clientes vista Web"
+
+
+
+);
+
+
+$breadcumbs_link = array(
+
+
+
+        "inicio" => "/",
+
+
+
+        "empresas" => "/entreprises.php"
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+$breadcumbs_name = array(
+
+
+
+    "inicio" => "Accueil",
+
+
+
+    "empresas" => "Entreprises"
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+?>
